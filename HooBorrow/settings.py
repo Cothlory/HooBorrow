00b8fa21c6ab495ad1b0937c0f066f4443ce8f5e
Chang Huang <cothlory@gmail.com>
2025-02-10 02:43:01 -0500

    delete MIDDLEWARE

--- HooBorrow/settings.py
+++ HooBorrow/settings.py
@@ -128,9 +128,6 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
