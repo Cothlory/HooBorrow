9d82666c9ba6da30a7acc41ea925954da96376a7
Chang Huang <cothlory@gmail.com>
2025-02-10 01:07:57 -0500

    change SQL to SQLite3

--- HooBorrow/settings.py
+++ HooBorrow/settings.py
@@ -76,14 +76,18 @@ WSGI_APPLICATION = 'HooBorrow.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'REMOVED',
-        'USER': 'a15',
-        'PASSWORD': 'REMOVED',
-        'HOST': 'localhost',
-        'PORT': '5433',
-    }
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #    'NAME': 'REMOVED',
+    #    'USER': 'a15',
+    #    'PASSWORD': 'REMOVED',
+    #    'HOST': 'localhost',
+    #    'PORT': '5433',
+    #}
 }
 
 
