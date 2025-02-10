53a6d6b8d74f6e3cbe89c5e011f55316ac223898
Chang Huang <cothlory@gmail.com>
2025-02-10 02:49:01 -0500

    fix allowed hosts

--- HooBorrow/settings.py
+++ HooBorrow/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'REMOVED'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hooborrow-d21a3fe76bb0.herokuapp.com']
+ALLOWED_HOSTS = ['hooborrow-f55f42e63294.herokuapp.com']
 
 
 # Application definition
