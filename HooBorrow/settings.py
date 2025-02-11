e92dc18139719b83b53f4339c4749bfd660c78c1
Jennifer Wu <emd2ty@virginia.edu>
2025-02-10 21:53:19 -0500

    configured to connect to django database

--- HooBorrow/settings.py
+++ HooBorrow/settings.py
@@ -77,9 +77,18 @@ WSGI_APPLICATION = 'HooBorrow.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        )
+        'local': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+        # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
     #    'NAME': 'REMOVED',
     #    'USER': 'a15',
