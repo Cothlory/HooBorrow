9148980370a2f931610d57743cb783258ddb137d
Chang Huang <cothlory@gmail.com>
2025-03-02 22:10:13 -0500

    fix error message displayed on login page; enable email OR username login

--- HooBorrow/settings.py
+++ HooBorrow/settings.py
@@ -108,6 +108,11 @@ ACCOUNT_FORMS = {
     'login': 'main.forms.CustomLoginForm',
 }
 
+ACCOUNT_LOGIN_METHODS = {'username', 'email'}
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_USERNAME_REQUIRED = True
+
 WSGI_APPLICATION = 'HooBorrow.wsgi.application'
 SITE_ID = 1
 
