38921d537bb6bd0efc3cb1f6b78c0d345fd5ffdb
Chang Huang <cothlory@gmail.com>
2025-03-02 22:10:13 -0500

    fix error message displayed on login page; enable email OR username login

--- main/forms.py
+++ main/forms.py
@@ -1,20 +1,19 @@
 from allauth.account.forms import LoginForm as AllAuthLoginForm
+from allauth.account.forms import SignupForm as AllAuthSignupForm
+from allauth.account.forms import ResetPasswordForm as AllAuthResetPasswordForm
 from django import forms
 
 class CustomLoginForm(AllAuthLoginForm):
-  login = forms.CharField(widget=forms.TextInput(attrs={
-    'placeholder': 'Username',
-    'autocomplete': 'username',
-    'id': 'id_login',
-    'class': 'form-control'
-  }))
-  password = forms.CharField(widget=forms.PasswordInput(attrs={
-    'placeholder': 'Password',
-    'autocomplete': 'current-password',
-    'id': 'id_password',
-    'class': 'form-control'
-  }))
-  remember = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={
-    'id': 'id_remember',
-    'class': 'form-check-input'
-  }))
+    login = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Username',
+            'autocomplete': 'username',
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Password',
+            'autocomplete': 'current-password',
+        })
+    )
+    remember = forms.BooleanField(required=False)
