2a0436349cd1ba35962be670b671d5cc01a0479e
Chang Huang <cothlory@gmail.com>
2025-03-04 15:17:32 -0500

    show custome Site with id in admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.sites.models import Site
+from django.contrib.sites.admin import SiteAdmin
+from .models import RegisteredSite
 
-# Register your models here.
+admin.site.unregister(Site)
+
+class CustomSiteAdmin(SiteAdmin):
+    list_display = ('id', 'name', 'domain')
+
+admin.site.register(RegisteredSite, CustomSiteAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.sites.models import Site
 
 # Create your models here.
+
+class RegisteredSite(Site):
+    class Meta:
+        proxy = True
+        ordering = ['id']
