2e6b071e5cbcbd8f3c7e75f282cc5a721528aaa9
Chang Huang <cothlory@gmail.com>
2025-03-04 01:50:35 -0500

    update Procfile

--- HooBorrow/settings.py
+++ HooBorrow/settings.py
@@ -86,8 +86,8 @@ AUTHENTICATION_BACKENDS = [
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'APP': {
-            'client_id': os.getenv('GOOGLE_CLIENT_ID'),
-            'secret': os.getenv('GOOGLE_CLIENT_SECRET'),
+            'client_id': os.getenv('GOOGLE_CLIENT_ID', ''),
+            'secret': os.getenv('GOOGLE_CLIENT_SECRET', ''),
             'key': ''
         }
     }
