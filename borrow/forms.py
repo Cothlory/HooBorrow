6940b3a7aa273a5f7f0022adcec4efb532f288fc
russoryan9 <nqt2bq@virginia.edu>
2025-03-27 17:23:30 -0400

    Add Category to forms

--- borrow/forms.py
+++ borrow/forms.py
@@ -4,12 +4,12 @@ from .models import SimpleItem, ComplexItem
 class SimpleItemForm(forms.ModelForm):
     class Meta:
         model = SimpleItem
-        fields = ['name', 'quantity', 'location', 'instructions', 'photo']
+        fields = ['name', 'quantity', 'location', 'instructions', 'photo', 'category']
 
 class ComplexItemForm(forms.ModelForm):
     class Meta:
         model = ComplexItem
-        fields = ['name', 'quantity', 'location', 'instructions', 'condition', 'photo']
+        fields = ['name', 'quantity', 'location', 'instructions', 'condition', 'photo', 'category']
 
 class QuantityForm(forms.Form):
     quantity = forms.IntegerField(min_value=1, label="Quantity", required=True)
