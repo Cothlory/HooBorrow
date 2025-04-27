00eee0787e3c88b698b104bb35f13725b142c1e6
Chang Huang <cothlory@gmail.com>
2025-04-27 01:22:25 -0400

    fix django-csp format

--- HooBorrow/settings.py
+++ HooBorrow/settings.py
@@ -35,15 +35,23 @@ if not DEBUG and 'test' not in sys.argv:
     CSRF_COOKIE_SECURE = True
     
     # Content security
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_BROWSER_XSS_FILTER = True
-    CSP_DEFAULT_SRC = ("'self'",)
-    CSP_SCRIPT_SRC = ("'self'", 'https://cdn.jsdelivr.net', 'https://code.jquery.com', 'https://stackpath.bootstrapcdn.com', 'https://cdnjs.cloudflare.com')
-    CSP_STYLE_SRC = ("'self'", 'https://cdn.jsdelivr.net', 'https://stackpath.bootstrapcdn.com', 'https://cdnjs.cloudflare.com', "'unsafe-inline'")
-    CSP_IMG_SRC = ("'self'", 'data:', '*.amazonaws.com')
-    CSP_FONT_SRC = ("'self'", 'https://cdn.jsdelivr.net', 'https://cdnjs.cloudflare.com')
-    CSP_CONNECT_SRC = ("'self'",)
-    CSP_FRAME_SRC = ("'self'", 'https://accounts.google.com')
+    CONTENT_SECURITY_POLICY = {'DIRECTIVES': {'connect-src': ("'self'",),
+                'default-src': ("'self'",),
+                'font-src': ("'self'",
+                             'https://cdn.jsdelivr.net',
+                             'https://cdnjs.cloudflare.com'),
+                'frame-src': ("'self'", 'https://accounts.google.com'),
+                'img-src': ("'self'", 'data:', '*.amazonaws.com'),
+                'script-src': ("'self'",
+                               'https://cdn.jsdelivr.net',
+                               'https://code.jquery.com',
+                               'https://stackpath.bootstrapcdn.com',
+                               'https://cdnjs.cloudflare.com'),
+                'style-src': ("'self'",
+                              'https://cdn.jsdelivr.net',
+                              'https://stackpath.bootstrapcdn.com',
+                              'https://cdnjs.cloudflare.com',
+                              "'unsafe-inline'")}}
 
 ALLOWED_HOSTS = [
     'localhost',
